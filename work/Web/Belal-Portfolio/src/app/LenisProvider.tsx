b79b13886e2705cb06e0b0ed73b0760fd7869072
Belal aboseada <belalaboseada@gmail.com>
2026-06-15 18:29:28 +0300

    fix deployment issue v6

--- work/Web/Belal-Portfolio/src/app/LenisProvider.tsx
+++ work/Web/Belal-Portfolio/src/app/LenisProvider.tsx
@@ -1,19 +1,8 @@
 'use client';
 
-import {ReactLenis} from 'lenis/react';
-import React, { useEffect, useState } from 'react';
+import { ReactLenis } from 'lenis/react';
 
 export const LenisProvider = ({ children }: { children: React.ReactNode }) => {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return (
     <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
       {children}
