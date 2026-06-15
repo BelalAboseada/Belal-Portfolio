0cb4dcf28fc5a2ca19df94f0111728de844be2c6
Belal aboseada <belalaboseada@gmail.com>
2026-06-15 05:50:06 +0300

    fix deployment issue

--- work/Web/Belal-Portfolio/next.config.ts
+++ work/Web/Belal-Portfolio/next.config.ts
@@ -1,10 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {},
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
